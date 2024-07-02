d04d9fcef96265ffa909aab1aa3cc4a952400e68
Sridhar <srid01234567@gmail.com>
2024-07-02 15:13:41 +0530

    Add description

--- execute_commands.sh
+++ execute_commands.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
 # cmds are used inside the back ticks inside the script
+# | is called pipe. Piping is used to send the output of one command as input of another command.
+# Here, the result of disk usage in human readable format (df -h) is piped as input to grep tmpfs
+# grep command searches for tmpfs in the input piped from the df -h
+
 
 var=`df -h | grep tmpfs`
 echo $var
